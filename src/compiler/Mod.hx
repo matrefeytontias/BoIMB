384b6a7ce7f9b99b459f9315a29573779177a176
matrefeytontias <mattias@refeyton.fr>
2017-02-02 04:29:15 +0100

    Fix handling directories as files in Mod

--- src/compiler/Mod.hx
+++ src/compiler/Mod.hx
@@ -1,6 +1,7 @@
 package compiler;
 
 import haxe.io.Path;
+import sys.FileSystem;
 import sys.io.File;
 import sys.io.FileInput;
 import sys.io.FileOutput;
@@ -24,24 +25,27 @@ class Mod
 		dirname = s[s.length - 1];
 		
 		// Find all .lua files
-		FileSystemExplorer.explore(path, process);
+		FileSystemExplorer.explore(path, findLuaFiles);
 	}
 	
 	// Doesn't matter if we return true for a file
-	private function process(p:String) : Bool
+	private function findLuaFiles(p:String) : Bool
 	{
-		var pathObject = new Path(p);
-		if(pathObject.ext.toLowerCase() == "lua")
+		if(!FileSystem.isDirectory(p))
 		{
-			if(pathObject.file.toLowerCase() == "main")
+			var pathObject = new Path(p);
+			if(pathObject.ext.toLowerCase() == "lua")
 			{
-				if(mainSource != null)
-					throw "cannot have more than one main.lua per mod.";
-				mainSource = new MainSource(p);
-				name = mainSource.getModName();
+				if(pathObject.file.toLowerCase() == "main")
+				{
+					if(mainSource != null)
+						throw "cannot have more than one main.lua per mod.";
+					mainSource = new MainSource(p);
+					name = mainSource.getModName();
+				}
+				else
+					requiredSources.push(new RequiredSource(p));
 			}
-			else
-				requiredSources.push(new RequiredSource(p));
 		}
 		return true;
 	}
