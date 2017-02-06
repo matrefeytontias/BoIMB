1c5be1f24e98791a2f8af0d33ecc091db041836f
matrefeytontias <mattias@refeyton.fr>
2017-02-06 16:07:34 +0100

    Add recursive delete of directory

--- src/FileSystemExplorer.hx
+++ src/FileSystemExplorer.hx
@@ -4,15 +4,30 @@ import sys.FileSystem;
 
 class FileSystemExplorer
 {
-	static public function explore(dir:String, process:String -> Bool)
+	static public function explore(dir:String, process:String -> Bool, ?postDir:String -> Void)
 	{
+		if(!FileSystem.isDirectory(dir))
+			return;
 		var content:Array<String> = FileSystem.readDirectory(dir);
 		for(e in content)
 		{
 			var filename = Path.addTrailingSlash(dir) + e;
 			
 			if(process(filename) && FileSystem.isDirectory(filename))
+			{
 				explore(filename + "/", process);
+				if(postDir != null)
+					postDir(dir);
+			}
 		}
+	}
+	
+	static public function deleteDirectory(dir:String)
+	{
+		explore(dir, function (f:String) : Bool
+			{
+				if(!FileSystem.isDirectory(f))
+					FileSystem.deleteFile(f);
+					return true; }, function (d:String) { FileSystem.deleteDirectory(d); });
 	}
 }
